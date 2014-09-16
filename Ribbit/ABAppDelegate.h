5080cf43b3e9f4a0cc18500983c38870f6c0368f
Christian Navarrete <idinkyy@gmail.com>
2014-09-16 15:33:53 -0700

    Initial Commit

---
+++ Ribbit/ABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABAppDelegate.h
+//  Ribbit
+//
+//  Created by HoodsDream on 9/16/14.
+//  Copyright (c) 2014 Asteroid Blues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by HoodsDream on 9/16/14.
+//  Copyright (c) 2014 Asteroid Blues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABAppDelegate class]));
+    }
+}
